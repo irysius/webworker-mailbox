2e7f9905913ee82776e9ce374b20112f52a146f0
irysius <lestersy@hotmail.com>
2018-08-11 01:20:08 -0400

    updating sample

--- worker/worker.ts
+++ worker/worker.ts
@@ -2,6 +2,6 @@ declare function importScripts(...urls: string[]): void;
 importScripts('mailbox-worker.js');
 
 logic(function (source) {
-    let func = new Function(source).bind(null);
+    let func = new Function(`"use strict";\r\n${source}`).bind(null);
     return Promise.resolve(func());
 });
